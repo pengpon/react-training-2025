294bed06362b808aa00e6f23e4af63a9d9e1d393
pengpon <bd102pon@gmail.com>
2026-01-09 19:01:19 +0800

    feat(w2): add axios instance

---
+++ week2/src/axiosInstance.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { getCookie } from "./utility";
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+
+const api = axios.create({
+  baseURL: API_BASE
+})
+
+// Request
+api.interceptors.request.use(
+  (config) => {
+    // check request URL
+    if (config.url.includes("/admin")) {
+      const token = getCookie("hexEcToken");
+      if (token) {
+        config.headers.Authorization = `${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
