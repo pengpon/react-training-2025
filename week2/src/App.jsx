823efce5d220160082c18828dd374927517c6a03
pengpon <bd102pon@gmail.com>
2026-01-10 16:16:18 +0800

    feat(w2): add product item modal component

--- week2/src/App.jsx
+++ week2/src/App.jsx
@@ -8,8 +8,8 @@ const API_PATH = import.meta.env.VITE_API_PATH;
 const App = () => {
   const [isAuth, setIsAuth] = useState(false);
   const [products, setProducts] = useState([]);
-
-  const getProducts = useCallback(async () => {
+  const [isModalShow, setIsModalShow] = useState(false);
+  const [productSelectedId, setProductSelectedId] = useState("");
     try {
       const res = await api.get(`/${API_PATH}/admin/products/all`);
       setProducts(Object.values(res.data.products));
@@ -17,6 +17,14 @@ const App = () => {
       console.error(error.message);
       if (error.status === 401) setIsAuth(false);
     }
+  };
+
+  const handleProductSelect = (e) => {
+    const selectedId = e.target.dataset.id;
+    setIsModalShow(true);
+    setProductSelectedId(selectedId);
+  };
+
   const Spinner = () => {
     return (
       <>
@@ -43,6 +51,103 @@ const App = () => {
             </svg>
             Loading...
           </button>
+        </div>
+      </>
+    );
+  };
+
+  const ProductItem = ({ id, closeModal }) => {
+    const [item] = products.filter((item) => item.id === id);
+    return (
+      <>
+        <div className="absolute top-0 left-0 bg-black opacity-70 w-full h-full "></div>
+        <div className="flex absolute left-1/2 top-1/2 -translate-1/2 w-lg items-center overflow-hidden bg-white px-4 pt-14 pb-8 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8 rounded-3xl">
+          <button
+            type="button"
+            className="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8"
+            onClick={closeModal}
+          >
+            <span className="sr-only">Close</span>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              data-slot="icon"
+              aria-hidden="true"
+              className="size-6"
+            >
+              <path
+                d="M6 18 18 6M6 6l12 12"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </button>
+
+          <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+            <div className="grid gap-3 sm:col-span-4 lg:col-span-5">
+              <img
+                src={item.imageUrl}
+                alt={item.title}
+                className="aspect-2/3 w-full rounded-lg bg-white object-contain"
+              />
+              <div className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-4">
+                {item.imagesUrl.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`image${index}`}
+                    className="rounded-lg bg-gray-100"
+                  />
+                ))}
+              </div>
+            </div>
+
+            <div className="sm:col-span-8 lg:col-span-7">
+              <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">
+                {item.title}
+              </h2>
+              <section aria-labelledby="information-heading" className="mt-2">
+                <h3 id="information-heading" className="sr-only">
+                  Product information
+                </h3>
+                <p className="mt-1 text-lg font-medium ">
+                  <span className="text-red-600 me-3">${item.price}</span>
+                  <span className="text-gray-600 line-through">
+                    ${item.origin_price}
+                  </span>
+                </p>
+              </section>
+              <section aria-labelledby="options-heading" className="mt-10">
+                <h3 id="options-heading" className="sr-only">
+                  Product options
+                </h3>
+                <div>
+                  <div className="mb-8">
+                    <h3 className="text-lg font-medium text-gray-900">
+                      產品描述：
+                    </h3>
+                    <div className="space-y-6">
+                      <p className="mt-4 text-sm text-gray-700">
+                        {item.description}
+                      </p>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="text-lg font-medium text-gray-900">
+                      食用方式：
+                    </h3>
+                    <div className="space-y-6">
+                      <p className="mt-4 text-sm text-gray-700">
+                        {item.content}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
         </div>
       </>
     );
@@ -198,7 +303,20 @@ const App = () => {
       </>
     );
   };
-  return <>{!isAuth ? <LoginForm /> : <ProductList products={products} />}</>;
+  return (
+    <>
+      {isLoading && <Spinner />}
+      {!isAuth ? <LoginForm /> : <ProductList products={products} />}
+      {isModalShow && (
+        <ProductItem
+          id={productSelectedId}
+          closeModal={() => {
+            setIsModalShow(false);
+          }}
+        />
+      )}
+    </>
+  );
 };
 
 export default App;
