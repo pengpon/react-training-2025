dca74bacfb9541976868850e1044ddbd4aa92d92
pengpon <bd102pon@gmail.com>
2026-01-10 16:13:03 +0800

    feat(w2): add spinner component

--- week2/src/App.jsx
+++ week2/src/App.jsx
@@ -17,12 +17,36 @@ const App = () => {
       console.error(error.message);
       if (error.status === 401) setIsAuth(false);
     }
-  }, []);
-
-  useEffect(() => {
-    getProducts();
-  }, [getProducts]);
-
+  const Spinner = () => {
+    return (
+      <>
+        <div className="absolute w-full h-full z-10 bg-white">
+          <button
+            type="button"
+            className="flex absolute left-1/2 top-1/2 transform -translate-x-1/2 translate-y-1/2 bg-sky-500 text-white p-2 rounded-md"
+            disabled
+          >
+            <svg className="mr-3 size-5 animate-spin" viewBox="0 0 24 24">
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Loading...
+          </button>
+        </div>
+      </>
+    );
+  };
 
   const ProductList = ({ products }) => {
     const Product = ({product}) => {
