180090d6d2ff0c6e7433eb9076608c7a0a29db9f
pengpon <bd102pon@gmail.com>
2026-01-15 15:14:35 +0800

    feat(w3): add action alert for delete product

--- week3/src/pages/ProductTableList.jsx
+++ week3/src/pages/ProductTableList.jsx
@@ -9,6 +9,48 @@ import {
 import { IconEdit, IconTrash, IconX, IconPlus } from "../components/Icons";
 import Spinner from "../components/Spinner";
 
+function ProductActionAlert({ selectedItem, handleAlertAction }) {
+  return (
+    <>
+      <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/60">
+        <div className="relative px-4 min-h-screen md:flex md:items-center md:justify-center">
+          <div className="bg-white rounded-lg md:max-w-md md:mx-auto p-4 fixed inset-x-0 bottom-0 z-50 mb-4 mx-4 md:relative shadow-lg">
+            <div className="md:flex items-center">
+              <div className="rounded-full border border-gray-300 flex items-center justify-center w-16 h-16 shrink-0 mx-auto">
+                <i className="bx bx-error text-3xl">&#9888;</i>
+              </div>
+              <div className="mt-4 md:mt-0 md:ml-6 text-center md:text-left">
+                <p className="font-bold">提醒！</p>
+                <p className="text-sm text-gray-700 mt-1">
+                  是否確定刪除 {selectedItem.title} 嗎？
+                </p>
+              </div>
+            </div>
+            <div className="text-center md:text-right mt-4 md:flex md:justify-end md:gap-2">
+              <button
+                type="button"
+                className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-red-200 text-red-700 rounded-lg font-medium text-sm md:ml-2 cursor-pointer"
+                data-confirm="true"
+                onClick={handleAlertAction}
+              >
+                刪除
+              </button>
+              <button
+                type="button"
+                className="block w-full md:inline-block md:w-auto px-4 py-3 md:py-2 bg-neutral-gray-light rounded-lg font-medium text-sm mt-4 md:mt-0 cursor-pointer"
+                data-confirm=""
+                onClick={handleAlertAction}
+              >
+                取消
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
 function ProductRow({ item, openModal, removeProduct }) {
   const {
     id,
@@ -287,8 +329,10 @@ function ProductItemModal({ selectedItem, onSubmit, closeModal, isNewItem }) {
 function ProductTableList() {
   const [isLoading, setIsLoading] = useState(true);
   const [isModalShow, setIsModalShow] = useState(false);
+  const [isActionAlertShow, setIsActionAlertShow] = useState(false);
   const [selectedProductItem, setSelectedProductItem] = useState({});
   const [productsData, setProductsData] = useState([]);
+
   const columnHeaders = ["名稱", "售價", "原價", "單位", "狀態", "動作"];
 
   const addNewProduct = async (data) => {
@@ -339,11 +383,27 @@ function ProductTableList() {
     handleModalShow();
   };
 
-  const handleRemoveProduct = async (e) => {
+  const handleActionAlert = (e) => {
+    const [selectedItem] = productsData.filter(
+      (product) => product.id === e.currentTarget.dataset.id
+    );
+    setSelectedProductItem(selectedItem);
     setIsLoading(true);
-    await removeProduct(e.currentTarget.dataset.id);
-    await getAllProducts();
-    setIsLoading(false);
+    setIsActionAlertShow(true);
+  };
+
+  const handleRemoveProduct = async (e) => {
+    setIsActionAlertShow(false);
+    const isDelete = e.currentTarget.dataset.confirm;
+    try {
+      if (isDelete) await removeProduct(selectedProductItem.id);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setSelectedProductItem({});
+      await getAllProducts();
+      setIsLoading(false);
+    }
   };
 
   const handleSubmit = async (e, formData) => {
@@ -420,7 +480,7 @@ function ProductTableList() {
                     item={item}
                     key={item.id}
                     openModal={handleSelectedModal}
-                    removeProduct={handleRemoveProduct}
+                    removeProduct={handleActionAlert}
                   />
                 ))
               )}
@@ -436,6 +496,12 @@ function ProductTableList() {
           )}
         </div>
       </div>
+      {isActionAlertShow && (
+        <ProductActionAlert
+          selectedItem={selectedProductItem}
+          handleAlertAction={handleRemoveProduct}
+        />
+      )}
     </>
   );
 }
