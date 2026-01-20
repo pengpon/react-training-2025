ab6dcc281318b6ecafd5ab72174f910322db5f73
pengpon <bd102pon@gmail.com>
2026-01-21 07:20:51 +0800

    feat(w4): add api service

---
+++ week4/src/api/products.js
@@ -0,0 +1,31 @@
+import axiosInstance from "./axiosInstance";
+
+const API_PATH = import.meta.env.VITE_API_PATH;
+
+// API
+// https://ec-course-api.hexschool.io
+// [GET] /v2/api/{api_path}/admin/products/all
+// [GET] /v2/api/{api_path}/admin/products  (query: page, category)
+// [POST] /v2/api/{api_path}/admin/product
+// [DELETE] /v2/api/{api_path}/admin/product/{id}
+// [PUT] /v2/api/{api_path}/admin/product/{id}
+
+export const fetchAllProducts = () =>
+  axiosInstance.get(`${API_PATH}/admin/products/all`);
+
+export const fetchProducts = (page = 1, category) =>
+  axiosInstance.get(`${API_PATH}/admin/products`, {
+    params: {
+      page: page,
+      category: category
+    },
+  });
+
+export const createProduct = (data) =>
+  axiosInstance.post(`${API_PATH}/admin/product`, { data });
+
+export const editProduct = (id, data) =>
+  axiosInstance.put(`${API_PATH}/admin/product/${id}`, { data });
+
+export const deleteProduct = (id) =>
+  axiosInstance.delete(`${API_PATH}/admin/product/${id}`);
