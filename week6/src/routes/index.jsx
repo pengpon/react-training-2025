e939188c95f66ed7e605eff0cf33ad54e280c0c8
pengpon <bd102pon@gmail.com>
2026-02-11 23:34:35 +0800

    fix(w6): use dynamic import

--- week6/src/routes/index.jsx
+++ week6/src/routes/index.jsx
@@ -1,20 +1,28 @@
-import FrontLayout from "../layouts/FrontLayout";
-import Login from "../views/Login";
-import NotFound from "../views/NotFound";
-import Index from "../views/front/Index";
-import ProductList from "../views/front/ProductList";
-import ProductItem from "../views/front/ProductItem";
-import Cart from "../views/front/Cart";
-import About from "../views/front/About";
-import Location from "../views/front/Location";
-import Blog from "../views/front/Blog";
-import Checkout from "../views/front/Checkout";
-import Payment from "../views/front/Payment";
-
-import AdminLayout from "../layouts/AdminLayout";
-import AdminProductList from "../views/admin/AdminProductList";
+import { lazy } from 'react';
 import { Navigate } from "react-router";
-import PaymentSuccess from "../views/front/PaymentSuccess";
+
+// Layouts
+const FrontLayout = lazy(() => import('../layouts/FrontLayout'));
+const AdminLayout = lazy(() => import('../layouts/AdminLayout'));
+
+// Front Views
+const Index = lazy(() => import('../views/front/Index'));
+const ProductList = lazy(() => import('../views/front/ProductList'));
+const ProductItem = lazy(() => import('../views/front/ProductItem'));
+const Cart = lazy(() => import('../views/front/Cart'));
+const About = lazy(() => import('../views/front/About'));
+const Location = lazy(() => import('../views/front/Location'));
+const Blog = lazy(() => import('../views/front/Blog'));
+const Checkout = lazy(() => import('../views/front/Checkout'));
+const Payment = lazy(() => import('../views/front/Payment'));
+const PaymentSuccess = lazy(() => import('../views/front/Payment'));
+// Admin Views
+const Login = lazy(() => import('../views/Login'));
+const AdminProductList = lazy(() => import('../views/admin/AdminProductList'));
+
+// Other Views
+const NotFound = lazy(() => import('../views/NotFound'));
+
 
 const routes = [
   {
