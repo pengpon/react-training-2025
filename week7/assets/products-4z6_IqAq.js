5317a30f81b48600a448898fc194b00b7cc90408
pengpon <bd102pon@gmail.com>
2026-03-03 15:35:21 +0800

    Updates

---
+++ week7/assets/products-4z6_IqAq.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index-DwXlESRM.js";const o="root",s=()=>t.get(`/api/${o}/products/all`),a=c=>t.get(`/api/${o}/product/${c}`);export{a,s as f};
--- week7/assets/products-DIUC5bj8.js
+++
@@ -1,1 +0,0 @@
-import{f as t}from"./index-C8jhmTRd.js";const o="root",s=()=>t.get(`/api/${o}/products/all`),a=c=>t.get(`/api/${o}/product/${c}`);export{a,s as f};
